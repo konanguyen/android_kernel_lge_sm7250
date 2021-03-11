ff152008aa647df0d263c0410633fc184e26d046
JeongHyeon Lee <jhs2.lee@samsung.com>
2021-03-11 21:10:50 +0900

    dm verity: add root hash pkcs#7 signature verification
    [ Upstream commit 88cd3e6cfac915f50f7aa7b699bdf053afec866e ]
    
    The verification is to support cases where the root hash is not secured
    by Trusted Boot, UEFI Secureboot or similar technologies.
    
    One of the use cases for this is for dm-verity volumes mounted after
    boot, the root hash provided during the creation of the dm-verity volume
    has to be secure and thus in-kernel validation implemented here will be
    used before we trust the root hash and allow the block device to be
    created.
    
    The signature being provided for verification must verify the root hash
    and must be trusted by the builtin keyring for verification to succeed.
    
    The hash is added as a key of type "user" and the description is passed
    to the kernel so it can look it up and use it for verification.
    
    Adds CONFIG_DM_VERITY_VERIFY_ROOTHASH_SIG which can be turned on if root
    hash verification is needed.
    
    Kernel commandline dm_verity module parameter 'require_signatures' will
    indicate whether to force root hash signature verification (for all dm
    verity volumes).
    
    Signed-off-by: Jaskaran Khurana <jaskarankhurana@linux.microsoft.com>
    Tested-and-Reviewed-by: Milan Broz <gmazyland@gmail.com>
    Signed-off-by: Mike Snitzer <snitzer@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/md/dm-verity-target.c
+++ drivers/md/dm-verity-target.c
@@ -34,8 +34,13 @@
 #define DM_VERITY_OPT_IGN_ZEROES	"ignore_zero_blocks"
 #define DM_VERITY_OPT_AT_MOST_ONCE	"check_at_most_once"
 
-#define DM_VERITY_OPTS_MAX		(2 + DM_VERITY_OPTS_FEC)
+<<<<<<< HEAD
+#define DM_VERITY_OPTS_MAX		(3 + DM_VERITY_OPTS_FEC)
 #define DM_VERITY_MEMORY_DUMP
+=======
+#define DM_VERITY_OPTS_MAX		(3 + DM_VERITY_OPTS_FEC)
+
+>>>>>>> 1410d3d7f470 (dm verity: add root hash pkcs#7 signature verification)
 static unsigned dm_verity_prefetch_cluster = DM_VERITY_DEFAULT_PREFETCH_SIZE;
 
 module_param_named(prefetch_cluster, dm_verity_prefetch_cluster, uint, S_IRUGO | S_IWUSR);
